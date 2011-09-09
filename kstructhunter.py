d2ed69122ca427151ab9f3e7c0540f6595d49f0c
Jon Oberheide <jon@oberheide.org>
2011-09-08 21:15:12 -0400

    improve kstructhunter option handling and include data files for stock ubuntu natty kernels

--- kstructhunter.py
+++ kstructhunter.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
+#
+# kstructhunter.py
+#
+# Jon Oberheide <jon@oberheide.org>
+# http://jon.oberheide.org
+#
+# Routines for hunting down kernel structs.
+#
+# Ships with data files for the stock Ubuntu Natty kernels.
+#
 
-import sys
+import os, sys, optparse, platform, gzip, bz2
 
 KMALLOC_CACHES = [
     8, 16, 32, 64, 96, 128, 192, 256, 512, 1024, 2048, 4096, 8192
 ]
 
-def main():
-    if len(sys.argv) != 3:
-        print 'usage: %s <data file> <struct name>' % (sys.argv[0])
-        sys.exit(1)
-
-    path, target = sys.argv[1:]
-
-    f = open(path)
-    data = f.readlines()
-    f.close()
-
+def hunt(data, target):
     structs = {}
     caches = {}
 
@@ -52,11 +52,49 @@ def main():
     size, cache = structs[target]
 
     print 'target struct %s is size %d' % (target, size)
+    print
     print 'target struct %s is allocated in kmalloc-%d' % (target, cache)
+    print
     print 'other structs allocated in kmalloc-%d:' % (cache)
 
     for struct in caches[cache]:
         print '\t%s' % struct
+
+def main():
+    usage = 'usage: %prog [options] struct_name'
+    opt = optparse.OptionParser(usage=usage)
+    opt.add_option('-f', '--file', dest='path', help='specify the path to a custom data file', metavar='PATH')
+    options, args = opt.parse_args()
+
+    if len(args) != 1:
+        opt.error('no target struct name was provided')
+
+    if options.path and not os.path.exists(options.path):
+        opt.error('invalid path to the custom data file')
+
+    bits, linkage = platform.architecture()
+
+    if options.path:
+        path = options.path
+    elif bits == '32bit':
+        path = 'structs-32.txt.gz'
+    elif bits == '64bit':
+        path = 'structs-64.txt.gz'
+    else:
+        opt.error('invalid architecture')
+
+    if path.endswith('.bz2'):
+        fopen = bz2.BZ2File
+    elif path.endswith('.gz'):
+        fopen = gzip.GzipFile
+    else:
+        fopen = file
+
+    f = fopen(path, 'rb')
+    data = f.readlines()
+    f.close()
+
+    hunt(data, args[0])
 
 if __name__ == '__main__':
     main()
